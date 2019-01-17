37e3cb0349097eff47cf27bf4c342a936ec28495
Student <Student@FORPCL151002BD.aaps.internal>
2019-01-16 19:46:01 -0500

    added robotmap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,6 +14,54 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
+  public static class CAN {
+    // Talons
+    public static final int DRIVE_LEFT_1 = 6;
+    public static final int DRIVE_LEFT_2 = 7;
+    public static final int INDENTURED_SERVANT_L = 8;
+    public static final int DRIVE_RIGHT_1 = 3;
+    public static final int DRIVE_RIGHT_2 = 4;
+    public static final int INDENTURED_SERVANT_R = 5;
+
+    public static final int CLIMB_TALON_1 = 1;
+    public static final int CLIMB_TALON_2 = 2;
+    // Solenoids
+    public static final int SHIFTER_1 = 3;
+    public static final int SHIFTER_2 = 2;
+
+    public static final int GEAR_HOLDER_1 = 0;
+    public static final int GEAR_HOLDER_2 = 1;
+
+    // DIGITAL INPUT
+    public static final int ENC_LEFT_1 = 0;
+    public static final int ENC_LEFT_2 = 1;
+    public static final int ENC_RIGHT_1 = 2;
+    public static final int ENC_RIGHT_2 = 3;
+
+    public static final int GEAR_SENSOR = 4;
+  }
+  
+  public static class XBOX {
+    // Buttons
+    public static final int BUTTON_A = 1;
+    public static final int BUTTON_B = 2;
+    public static final int BUTTON_X = 3;
+    public static final int BUTTON_Y = 4;
+    public static final int BUMPER_LEFT = 5;
+    public static final int BUMPER_RIGHT = 6;
+    public static final int BUTTON_BACK = 7;
+    public static final int BUTTON_START = 8;
+    public static final int STICK_LEFT = 9;
+    public static final int STICK_RIGHT = 10;
+
+    // Axes
+    public static final int STICK_L_X_AXIS = 0;
+    public static final int STICK_L_Y_AXIS = 1;
+    public static final int STICK_R_X_AXIS = 4;
+    public static final int STICK_R_Y_AXIS = 5;
+    public static final int TRIGGER_L_AXIS = 2;
+    public static final int TRIGGER_R_AXIS = 3;
+  }
   // For example to map the left and right motors, you could define the
   // following variables to use with your drivetrain subsystem.
   // public static int leftMotor = 1;
