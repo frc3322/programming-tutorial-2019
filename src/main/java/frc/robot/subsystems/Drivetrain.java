d2b325abe466df1fe6b3292c91bd5f501433bec9
Programming 1 <2022helgesonthorw@aapd.k12.mi.us>
2019-01-16 19:32:24 -0500

    Add drivetrain file

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+import frc.robot.commands.DriveControl;
+
+public class Drivetrain extends Subsystem {
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+}
