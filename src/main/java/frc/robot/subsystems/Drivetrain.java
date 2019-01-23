be642db7f0480e031beab1b3de008d1bb728fcfb
Jonathan Park <joohopark27@gmail.com>
2019-01-22 19:43:34 -0500

    simplified drivetrain

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,21 +1,25 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.Robot;
 import frc.robot.RobotMap;
 import frc.robot.commands.DriveControl;
 
 public class Drivetrain extends Subsystem {
-
+    public Drivetrain(){
+        
+    }
     DifferentialDrive robotDrive;
 
-    public Drivetrain(){
+    Drivetrain(double lowThreshold, double highThreshold, int numSamples, int cooldown, boolean left_inv, boolean right_inv){
     
-        WPI_TalonSRX left1 = new WPI_TalonSRX(frc.robot.RobotMap.CAN.DRIVE_LEFT_1);
+        var left1 = new WPI_TalonSRX(frc.robot.RobotMap.CAN.DRIVE_LEFT_1);
         WPI_TalonSRX left2 = new WPI_TalonSRX(RobotMap.CAN.DRIVE_LEFT_2);
         WPI_TalonSRX left3 = new WPI_TalonSRX(RobotMap.CAN.INDENTURED_SERVANT_L);
         WPI_TalonSRX right1 = new WPI_TalonSRX(RobotMap.CAN.DRIVE_RIGHT_1);
@@ -26,8 +30,8 @@ public class Drivetrain extends Subsystem {
         SpeedControllerGroup rightGroup = new SpeedControllerGroup(right1, right2);
         left3.follow(left1);
         right3.follow(right1);
-
-        left1.setInverted(left_inv);
+  
+       	left1.setInverted(left_inv);
         left2.setInverted(left_inv);
         left3.setInverted(left_inv);
         right1.setInverted(right_inv);
